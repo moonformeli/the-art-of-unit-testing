67160ff9c79a799062a1d26276b24726d050dd03
moonformeli <mgyang95@gmail.com>
2024-05-31 01:50:26 +0900

    refactor: 예제 1 parseInt -> Number.parseInt로 변경

--- ch1-basics/number-parser.js
+++ ch1-basics/number-parser.js
@@ -7,7 +7,7 @@
  */
 const sum = (numbers) => {
   const [a, b] = numbers.split(',');
-  const result = parseInt(a, 10) + parseInt(b, 10);
+  const result = Number.parseInt(a, 10) + Number.parseInt(b, 10);
   return result;
 };
 
