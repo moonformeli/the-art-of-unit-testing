e8ad411e550b7f0bb8680cbd3457da989f13cd5f
moonformeli <mgyang95@gmail.com>
2024-08-27 02:24:00 +0900

    feat: 부록 00-begin 코드 추가

---
+++ appendix/00-begin/machine-scanner.spec.js
@@ -0,0 +1,19 @@
+const { findRecentlyRebooted } = require('./machine-scanner');
+
+describe('findRecentlyRebooted', () => {
+  test('given no machines, returns empty results', () => {
+    const result = findRecentlyRebooted([], 0);
+
+    expect(result.length).toBe(0);
+  });
+
+  test('given one machine over the threshold, it is ignored', () => {
+    const DAYS_IN_MS = 1000 * 60 * 60 * 24;
+    const twoDaysAgo = Date.now() - 2 * DAYS_IN_MS;
+    const machines = [{ lastBootTime: twoDaysAgo, name: 'machine1' }];
+
+    const result = findRecentlyRebooted(machines, 1);
+
+    expect(result.length).toBe(0);
+  });
+});
